dd4028e87539eb1860c9f09d66c4583ee9bbec8b
Molikkidd <mzkidd5@gmail.com>
2024-05-25 16:52:18 -0400

    initial commit

---
+++ src/main/java/com/buildmo/aws_image_upload/AwsImageUploadApplication.java
@@ -0,0 +1,13 @@
+package com.buildmo.aws_image_upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsImageUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsImageUploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buildmo/aws_image_upload/AwsImageUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildmo.aws_image_upload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsImageUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
