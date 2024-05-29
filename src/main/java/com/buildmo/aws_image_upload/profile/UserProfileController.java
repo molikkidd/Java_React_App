b363399b5ba8872f65ad93d69d6e7215a14b17cb
Molikkidd <mzkidd5@gmail.com>
2024-05-29 16:40:31 -0400

    rename app to main and add service annotation to userprofile service

---
+++ src/main/java/com/buildmo/aws_image_upload/profile/UserProfileController.java
@@ -0,0 +1,23 @@
+package com.buildmo.aws_image_upload.profile;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/user-profile")
+public class UserProfileController {
+
+    private final UserProfileService userProfileService;
+
+    public UserProfileController(UserProfileService userProfileService) {
+        this.userProfileService = userProfileService;
+    }
+
+    @GetMapping
+    public List<UserProfile> getUserProfiles(){
+        return userProfileService.getUserProfiles();
+    }
+ }
