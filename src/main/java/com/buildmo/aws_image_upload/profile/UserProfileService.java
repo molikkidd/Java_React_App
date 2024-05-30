30c3e57561fa8fe105c4c363def1d158b4f4f908
Molikkidd <mzkidd5@gmail.com>
2024-05-30 15:12:35 -0400

    update Userprofileservice to accept and upload and image to the amazon s3 bucket and add error handling

--- src/main/java/com/buildmo/aws_image_upload/profile/UserProfileService.java
+++ src/main/java/com/buildmo/aws_image_upload/profile/UserProfileService.java
@@ -1,19 +1,26 @@
 package com.buildmo.aws_image_upload.profile;
 
+import com.buildmo.aws_image_upload.bucket.BucketName;
+import com.buildmo.aws_image_upload.filestore.FileStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-import java.util.UUID;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.*;
+
+import static org.springframework.http.MediaType.*;
 
 @Service
 public class UserProfileService {
     private final UserProfileDataAccessService userProfileDataAccessService;
+    private final FileStore fileStore;
 
     @Autowired
-    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService) {
+    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService, FileStore fileStore) {
         this.userProfileDataAccessService = userProfileDataAccessService;
+        this.fileStore = fileStore;
     }
 
     List<UserProfile> getUserProfiles() {
@@ -22,10 +29,31 @@ public class UserProfileService {
 
     public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
 //        check if image is not empty
+        if(file.isEmpty()){
+            throw  new IllegalStateException("Cannot upload empty file [ " + file.getSize() + "]");
+        }
 //        If file is an image
+        if(!Arrays.asList(IMAGE_JPEG, IMAGE_PNG, IMAGE_GIF).contains(file.getContentType())){
+            throw new IllegalStateException("File must be an image");
+        }
 //        The user exists in our database
+        UserProfile user = userProfileDataAccessService
+                .getUserProfiles()
+                .stream()
+                .filter(userProfile -> userProfile.getUserProfileId().equals(userProfileId))
+                .findFirst()
+                .orElseThrow(()-> new IllegalStateException(String.format("User Profile %s not found", userProfileId)));
 //        Grab some metadata form file if any
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("Content-Type",  file.getContentType());
+        metadata.put("Content-Length", String.valueOf(file.getSize()));
 //        Store the image in s# and update database with s3 image link
+        String path = String.format("%s/%s", BucketName.PROFILE_IMAGE.getBucketName(), user.getUserProfileId());
+        String filename = String.format("%s/%s", file.getName(), UUID.randomUUID());
+        try {
+            fileStore.save(path, filename,Optional.of(metadata), file.getInputStream());
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
     }
-
 }
