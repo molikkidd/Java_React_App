2000041ad565a26f771b01087dd5a6b5c78f7da4
Molikkidd <mzkidd5@gmail.com>
2024-05-30 11:49:42 -0400

    Add React front end library and connect backend with axios

--- src/main/java/com/buildmo/aws_image_upload/profile/UserProfileService.java
+++ src/main/java/com/buildmo/aws_image_upload/profile/UserProfileService.java
@@ -2,8 +2,10 @@ package com.buildmo.aws_image_upload.profile;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserProfileService {
@@ -17,4 +19,13 @@ public class UserProfileService {
     List<UserProfile> getUserProfiles() {
         return userProfileDataAccessService.getUserProfiles();
     }
+
+    public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+//        check if image is not empty
+//        If file is an image
+//        The user exists in our database
+//        Grab some metadata form file if any
+//        Store the image in s# and update database with s3 image link
+    }
+
 }
